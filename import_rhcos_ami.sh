da017e15fbfd21edb2189c24525601df78c09f8d
Dan Clark <danclark@redhat.com>
2023-10-16 10:29:07 -0400

    Updates to support SBE and non-SBE AWS regions. Fix typos

--- import_rhcos_ami.sh
+++ import_rhcos_ami.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# This script is intended to be run on the OpenShift Content Instance inside the AWS SBE
+# It will load the RHCOS RAW Disk Image file from S3 on the SBE into an AMI on the SBE
+# to be used when installing the OpenShift cluster on the SBE.
+
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -6,20 +11,32 @@ source "${SCRIPT_DIR}/env.sh"
 
 function usage {
 
-  echo "$0 <s3_bucket>"
-
+  echo "Usage: $0 <s3_bucket> [volume_type]"
 }
 
-# TODO: Check if enough args
-S3_BUCKET=${1}
+if [[ $# < 1 ]]
+then
+  usage
+  exit 1
+fi
+
+S3_BUCKET="${1}"
+
+# Default to SBE volume type
+VOL_TYPE="sbp1"
+if [[ $# == 2 ]]
+then
+  VOL_TYPE="${2}"
+fi
 
 ##############################################
 # RHCOS Snapshot
 
+#Copy the RHCOS RAW disk from the SBE into S3 on the SBE
 ${S3} cp "${RHCOS_IMG}" "s3://${S3_BUCKET}/"
 
+# Create required config file for EC2 snapshot import process
 rm -f /tmp/containers.json
- 
 cat << EOF > /tmp/containers.json
 { 
     "Description": "Red Hat CoreOS ${RHCOS_VER}",
@@ -36,6 +53,8 @@ RHCOS_IMPORT_ID=$( ${EC2} import-snapshot --disk-container "file:///tmp/containe
 echo "RHCOS Snapshot import ID: ${RHCOS_IMPORT_ID}"
 
 x="unknown"
+echo "Snapshot import takes 5-10 minutes in most cases"
+date
 while [[ "$x" != "completed" ]]
 do
   echo "Waiting for snapshot import to complete"
@@ -54,7 +73,7 @@ RHCOS_AMI=$(${EC2} register-image \
   --output text \
   --name "rhcos-${RHCOS_VER}" \
   --description "rhcos-${RHCOS_VER}" \
-  --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"SnapshotId\":\"${RHCOS_SNAPSHOT}\",\"VolumeType\":\"sbp1\",\"DeleteOnTermination\":true}}]" \
+  --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"SnapshotId\":\"${RHCOS_SNAPSHOT}\",\"VolumeType\":\"${VOL_TYPE}\",\"DeleteOnTermination\":true}}]" \
   --root-device-name /dev/sda1)
 
 echo "RHCOS AMI: ${RHCOS_AMI}"
