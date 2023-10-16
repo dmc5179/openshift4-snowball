a5962f1a7bb94381fa8e6d123d57e531a174ad65
Dan Clark <danclark@redhat.com>
2023-10-16 10:24:08 -0400

    Updates to support SBE and non-SBE AWS regions. Fix typos

--- import_content_ami.sh
+++ import_content_ami.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
-
+#
+# This script is intended to be run from outside the AWS SBE device.
+# It is used to import the OpenShift Content RAW Disk located in S3 on the SBE
+# Once the OpenShift Content AMI is loaded and instance running, everything else is run from the content AMI
+#
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source "${SCRIPT_DIR}/env.sh"
 
 function usage {
 
-  echo "$0 <s3_bucket> <path_to_ami_raw_disk_file_in_s3_bucket>"
-
+  echo "Usage: $0 <s3_bucket> <path_to_ami_raw_disk_file_in_s3_bucket> [volume_type]"
 }
 
-S3_BUCKET=${1}
-S3_PATH=${2}
+if [[ $# < 2 ]]
+then
+  usage
+  exit 1
+fi
+
+S3_BUCKET="${1}"
+S3_PATH="${2}"
+
+# Default to SBE volume type
+VOL_TYPE="sbp1"
+if [[ $# == 3 ]]
+then
+  VOL_TYPE="${3}"
+fi
 
 ##############################################
-# RHCOS Snapshot
+# Content Snapshot
 
-#${S3} cp "${RHCOS_IMG}" "s3://${S3_BUCKET}/"
-
+# Create file required by the AWS import snapshot command
 rm -f /tmp/containers.json
  
 cat << EOF > /tmp/containers.json
@@ -31,32 +46,40 @@ cat << EOF > /tmp/containers.json
 }
 EOF
 
+# Import the snapshot and get the import task ID
+# TODO: This command did not capture the import snapshot task ID
 CONTENT_IMPORT_ID=$( ${EC2} import-snapshot --disk-container "file:///tmp/containers.json" |  jq -r '.ImportTaskId')
 
-echo "Content Snapshot import ID: ${RHCOS_IMPORT_ID}"
+echo "Content Snapshot import ID: ${CONTENT_IMPORT_ID}"
 
+# Wait for the snapshot import to complete
 x="unknown"
+date
 while [[ "$x" != "completed" ]]
 do
   echo "Waiting for snapshot import to complete"
-  x=$(${EC2} describe-import-snapshot-tasks --import-task-ids ${RHCOS_IMPORT_ID} | jq -r '.ImportSnapshotTasks[0].SnapshotTaskDetail.Status')
+  x=$(${EC2} describe-import-snapshot-tasks --import-task-ids ${CONTENT_IMPORT_ID} | jq -r '.ImportSnapshotTasks[0].SnapshotTaskDetail.Status')
   sleep 5
 done
+date
 
-CONTENT_SNAPSHOT=$(${EC2} describe-import-snapshot-tasks --import-task-ids ${RHCOS_IMPORT_ID} | jq -r '.ImportSnapshotTasks[0].SnapshotTaskDetail.SnapshotId')
+# Get the imported snapshot ID
+CONTENT_SNAPSHOT=$(${EC2} describe-import-snapshot-tasks --import-task-ids ${CONTENT_IMPORT_ID} | jq -r '.ImportSnapshotTasks[0].SnapshotTaskDetail.SnapshotId')
 
-echo "Content snapshot ID: ${RHCOS_SNAPSHOT}"
+echo "Content snapshot ID: ${CONTENT_SNAPSHOT}"
 
+# Wait between import finish and register AMI
+# running these too fast sometimes results in an error
 sleep 5
 
-# Register the AMI
-RHCOS_AMI=$(${EC2} register-image \
+# Register the AMI using the imported snapshot and get the new AMI ID
+CONTENT_AMI=$(${EC2} register-image \
   --output text \
   --name "ocp4-${OCP_VER}-content" \
   --description "ocp4-${OCP_VER}-content" \
-  --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"SnapshotId\":\"${RHCOS_SNAPSHOT}\",\"VolumeType\":\"sbp1\",\"DeleteOnTermination\":true}}]" \
+  --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"SnapshotId\":\"${CONTENT_SNAPSHOT}\",\"VolumeType\":\"${VOL_TYPE}\",\"DeleteOnTermination\":true}}]" \
   --root-device-name /dev/sda1)
 
-echo "Content AMI: ${RHCOS_AMI}"
+echo "Content AMI: ${CONTENT_AMI}"
 
 exit 0
