092afa204eeec4b46abfa9dcb60607039ba79296
Dan Clark <danclark@redhat.com>
2023-10-30 19:16:34 -0400

    Helper script to deploy the httpd server locally

--- deploy_dns.sh
+++ deploy_dns.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+source "${SCRIPT_DIR}/env.sh"
+
+# Deploy httpd server to host ignition configs
+# This is due to EC2 user data size limitations
+echo "Installing HTTPD"
+
+pushd "${SCRIPT_DIR}/playbooks"
+
+ansible-playbook httpd_server.yaml
+
+popd
