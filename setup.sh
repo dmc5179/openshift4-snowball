f3bba3e1182bc8107cdf6fe61db10149d31458d3
Dan Clark <danclark@redhat.com>
2023-11-04 01:37:14 +0000

    Set python interpreter to 3.9 since RHEL 9 includes 3.11

--- setup.sh
+++ setup.sh
@@ -7,25 +7,32 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source "${SCRIPT_DIR}/env.sh"
 
+read -p "Deploy local DNS? (y/n): " dns
+
 # Deploy httpd server
 echo "Deploying httpd server"
 pushd "${SCRIPT_DIR}/playbooks"
-ansible-playbook httpd_server.yaml
+ansible-playbook -e ansible_python_interpreter=/usr/bin/python3.9 httpd_server.yaml
 popd
 
 # Deploy DNS
-echo "Deploying DNS server"
-pushd "${SCRIPT_DIR}/playbooks"
-subnet=$(hostname -i | awk -F\. '{print $1"."$2"."$3}')
-ansible-playbook \
-  --extra-vars "dns_zone_one=${BASE_DOMAIN} bastion_ip=$(hostname -i) dns_network=${subnet} bind_forwarder1=34.223.14.129 bind_forwarder2=8.8.8.8" \
-  dns_server.yaml
-popd
+if [[ $dns == "y" || $dns == "Y" || $dns == "yes" ]]
+then
+  echo "Deploying DNS server"
+  pushd "${SCRIPT_DIR}/playbooks"
+  subnet=$(hostname -i | awk -F\. '{print $1"."$2"."$3}')
+  ansible-playbook \
+    --extra-vars "ansible_python_interpreter=/usr/bin/python3.9 dns_zone_one=${BASE_DOMAIN} bastion_ip=$(hostname -i) dns_network=${subnet} bind_forwarder1=34.223.14.129 bind_forwarder2=8.8.8.8" \
+    dns_server.yaml
+  popd
+else
+  echo "Skipping local DNS deployment"
+fi
 
 # Deploy Load Balancer
 echo "Deploying Load Balancer"
 pushd "${SCRIPT_DIR}/playbooks"
 ansible-playbook \
-  --extra-vars "bootstrap_ip=${BOOTSTRAP_IP} master0_ip=${MASTER0_IP} master1_ip=${MASTER1_IP} master2_ip=${MASTER2_IP}" \
+  --extra-vars "ansible_python_interpreter=/usr/bin/python3.9 bootstrap_ip=${BOOTSTRAP_IP} master0_ip=${MASTER0_IP} master1_ip=${MASTER1_IP} master2_ip=${MASTER2_IP}" \
   load_balancer.yaml
 popd
