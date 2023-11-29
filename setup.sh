8d128993b62d71542705cd9db938f9a90e5c2a1c
Dan Clark <danclark@redhat.com>
2023-11-28 20:28:27 -0500

    Fix hostname parameter to be portable

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ if [[ $dns == "y" || $dns == "Y" || $dns == "yes" ]]
 then
   echo "Deploying DNS server"
   pushd "${SCRIPT_DIR}/playbooks"
-  subnet=$(hostname -i | awk -F\. '{print $1"."$2"."$3}')
+  subnet=$(hostname -I | awk -F\. '{print $1"."$2"."$3}')
   ansible-playbook \
     --extra-vars "ansible_python_interpreter=/usr/bin/python3.9 dns_zone_one=${BASE_DOMAIN} bastion_ip=$(hostname -i) dns_network=${subnet} bind_forwarder1=34.223.14.129 bind_forwarder2=8.8.8.8" \
     dns_server.yaml
