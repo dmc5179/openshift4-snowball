0875fb6f7de61ec5fbe6043686936f9d57011c47
Dan Clark <danclark@redhat.com>
2024-01-24 08:32:42 -0500

    Add loops to handle multiple instance returns

--- destroy_cluster.sh
+++ destroy_cluster.sh
@@ -14,6 +14,7 @@ fi
 
 echo "Collecting OpenShift cluster instance IDs..."
 
+# TODO: This doesn't handle when old OCP nodes are in the terminated state but still show in the describe-instances call
 BOOTSTRAP_ID=$($EC2 describe-instances | jq -r -c '.Reservations[].Instances[] | select(.Tags[0].Value=="Bootstrap").InstanceId')
 
 MASTER0_ID=$($EC2 describe-instances | jq -r -c '.Reservations[].Instances[] | select(.Tags[0].Value=="Master0").InstanceId')
@@ -26,21 +27,31 @@ if [[ ! -z "${BOOTSTRAP_ID}" ]]
 then
 
   echo "Destroying Bootstrap Instance"
-  $EC2 terminate-instances --instance-ids "${BOOTSTRAP_ID}"
+  for i in ${BOOTSTRAP_ID}
+  do
+    $EC2 terminate-instances --instance-ids "${i}"
+  done
 fi
 
 if [[ ! -z "${MASTER0_ID}" ]]
 then
 
   echo "Destroying Master 0 Instance"
-  $EC2 terminate-instances --instance-ids "${MASTER0_ID}"
+  for i in ${MASTER0_ID}
+  do
+    $EC2 terminate-instances --instance-ids "${i}"
+  done
+
 fi
 
 if [[ ! -z "${MASTER1_ID}" ]]
 then
 
   echo "Destroying Master 1 Instance"
-  $EC2 terminate-instances --instance-ids "${MASTER1_ID}"
+  for i in ${MASTER1_ID}
+  do
+    $EC2 terminate-instances --instance-ids "${i}"
+  done
 
 fi
 
@@ -48,7 +59,10 @@ if [[ ! -z "${MASTER2_ID}" ]]
 then
 
   echo "Destroying Master 2 Instance"
-  $EC2 terminate-instances --instance-ids "${MASTER2_ID}"
+  for i in ${MASTER2_ID}
+  do
+    $EC2 terminate-instances --instance-ids "${i}"
+  done
 
 fi
 
